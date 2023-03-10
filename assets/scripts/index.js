868d61c5e1ea319d831c08dea943b9f21bae6dd6
Hector <hecse71@gmail.com>
2023-03-10 09:34:11 -0300

    mas arregle target blank y se borro (nuevo)

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,20 +7,31 @@ for (let event of events) {
     <img src= ${event.image} class="card-img-top" alt="Costume Party">
     <div class="card-body">
         <h5 class="card-title"> ${event.name} </h5>
-        <p class="card-text"> ${event.description} </p>
-        <p class="card-text">Date: ${event.date} </p>
+        <p class="card-text"> ${event.description} </p>        
+    </div>
+    
+    <div class="row">
+        <div class="column col-12">        
+            <div class="card-footer bg-transparent">
+            Date:  ${event.date} 
+            </div>
+        </div>        
     </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="column col-8">
-                <div class="card-footer bg-transparent border-success">Price: $ ${event.price} </div>
-            </div>
+    <div class="row">
+        <div class="column col-8">
+            <div class="row">
+                <div class="column col-12">
+                <p class="card-text">Price: $ ${event.price} </p>
+                </div>
+            </div>                        
+        </div>
 
-            <div class="column col-4"><a href="./details.html" class="btn btn-primary">Details</a>
-            </div>
+        <div class="column col-4">
+            <a href="./details.html" class="btn btn-primary mb-1">Details</a>
         </div>
     </div>
+    
     </div>`
 }
 
--- assets/scripts/upcoming_events.js
+++ assets/scripts/upcoming_events.js
@@ -3,29 +3,37 @@ let tarjeta = ``
 
 
 for (let event of events) {
-    tarjeta += `<div class="card text m-2 p-0" style="width: 18rem;"> 
-    <img src= ${event.image} class="card-img-top" alt="Costume Party">
-    <div class="card-body">
-        <h5 class="card-title"> ${event.name} </h5>
-        <p class="card-text"> ${event.description} </p>
-        <p class="card-text">Date: ${event.date} </p>
+    if (event.date >= "2022-01-01") {
+        tarjeta += `<div class="card text m-2 p-0" style="width: 18rem;"> 
+          <img src= ${event.image} class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title"> ${event.name} </h5>
+                <p class="card-text"> ${event.description} </p>
+            </div>
+   
+            <div class="row">
+                <div class="column col-8">
+                    <div class="row">
+                       <div class="column col-12">
+                       <p class="card-text">Date: ${event.date} </p>
+                </div>
+            
+            <div class="row">
+                <div class="column col-12">
+                <div class="card-footer bg-transparent border-success"> Price: $ ${event.price} 
+                </div>
+            </div>            
+        </div>        
     </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="column col-8">
-                <div class="card-footer bg-transparent border-success">Price: $ ${event.price} </div>
-            </div>
-
-            <div class="column col-4"><a href="./details.html" class="btn btn-primary">Details</a>
-            </div>
+    </div>
+        <div class="column col-4"><a href="./details.html" class="btn btn-primary">Details</a>
         </div>
     </div>
+    
     </div>`
+    }
 }
 
 contenidoCard.innerHTML = tarjeta
 /* console.log(tarjeta.name) */
-
-
-
